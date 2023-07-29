5f6dccdc6449abdad381ee1e40b7332f11f59966
Michael Schulz <michael.schulz.mail@gmail.com>
2023-07-29 16:43:43 +0200

    Moved region 4 to an own module

--- src/iapws97.rs
+++ src/iapws97.rs
@@ -4,8 +4,11 @@ pub mod iapws97 {
     mod region_1;
     mod region_2;
     mod region_3;
+    mod region_4;
     use crate::iapws97::region_1::{cp_tp_1, cv_tp_1, h_tp_1, s_tp_1, u_tp_1, v_tp_1, w_tp_1};
     use crate::iapws97::region_2::{cp_tp_2, cv_tp_2, h_tp_2, s_tp_2, u_tp_2, v_tp_2, w_tp_2};
+    #[allow(unused_imports)]
+    use crate::iapws97::region_4::{psat97, tsat97};
 
     pub enum Region {
         Region1,
@@ -232,68 +235,6 @@ pub mod iapws97 {
             Region::Region2 => Ok(w_tp_2(t, p)),
             _ => Err(IAPWSError::NotImplemented()),
         }
-    }
-
-    // ================    Region 4 ===================
-
-    const REGION_4_SATURATION_COEFFS: [f64; 10] = [
-        0.11670521452767e4,
-        -0.72421316703206e6,
-        -0.17073846940092e2,
-        0.12020824702470e5,
-        -0.32325550322333e7,
-        0.14915108613530e2,
-        -0.48232657361591e4,
-        0.40511340542057e6,
-        -0.23855557567849,
-        0.65017534844798e3,
-    ];
-
-    /// Returns the saturation pressure in Pa
-    /// Temperature is assumed to be in K
-    pub fn psat97(t: f64) -> f64 {
-        let n1 = REGION_4_SATURATION_COEFFS[0];
-        let n2 = REGION_4_SATURATION_COEFFS[1];
-        let n3 = REGION_4_SATURATION_COEFFS[2];
-        let n4 = REGION_4_SATURATION_COEFFS[3];
-        let n5 = REGION_4_SATURATION_COEFFS[4];
-        let n6 = REGION_4_SATURATION_COEFFS[5];
-        let n7 = REGION_4_SATURATION_COEFFS[6];
-        let n8 = REGION_4_SATURATION_COEFFS[7];
-        let n9 = REGION_4_SATURATION_COEFFS[8];
-        let n10 = REGION_4_SATURATION_COEFFS[9];
-
-        let theta = t + n9 / (t - n10);
-
-        let coef_a = theta * theta + n1 * theta + n2;
-        let coef_b = n3 * theta * theta + n4 * theta + n5;
-        let coef_c = n6 * theta * theta + n7 * theta + n8;
-        (2.0 * coef_c / (-coef_b + (coef_b * coef_b - 4.0 * coef_a * coef_c).sqrt())).powi(4) * 1e6
-    }
-
-    /// Returns the saturation temperature in K
-    /// Pressure is assumed to be in Pa
-    pub fn tsat97(p: f64) -> f64 {
-        let n1 = REGION_4_SATURATION_COEFFS[0];
-        let n2 = REGION_4_SATURATION_COEFFS[1];
-        let n3 = REGION_4_SATURATION_COEFFS[2];
-        let n4 = REGION_4_SATURATION_COEFFS[3];
-        let n5 = REGION_4_SATURATION_COEFFS[4];
-        let n6 = REGION_4_SATURATION_COEFFS[5];
-        let n7 = REGION_4_SATURATION_COEFFS[6];
-        let n8 = REGION_4_SATURATION_COEFFS[7];
-        let n9 = REGION_4_SATURATION_COEFFS[8];
-        let n10 = REGION_4_SATURATION_COEFFS[9];
-
-        let beta = (p / 1e6).powf(0.25);
-
-        let coef_e = beta * beta + n3 * beta + n6;
-        let coef_f = n1 * beta * beta + n4 * beta + n7;
-        let coef_g = n2 * beta * beta + n5 * beta + n8;
-
-        let coef_d = 2.0 * coef_g / (-coef_f - (coef_f * coef_f - 4.0 * coef_e * coef_g).sqrt());
-
-        (n10 + coef_d - ((n10 + coef_d).powi(2) - 4.0 * (n9 + n10 * coef_d)).sqrt()) / 2.0
     }
 
     #[cfg(test)]
